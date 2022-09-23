e52cd42b82efb3a11445f2242bde9c6916e510aa
Jaewon <wellbird@naver.com>
2022-09-23 09:16:59 +0900

    12925 - 문자열을 정수로 바꾸기

---
+++ Level1/12925.js
@@ -0,0 +1,18 @@
+function solution(s) {
+  return Number(s);
+}
+
+// 다른사람의 풀이 1
+// function strToInt(str){
+//     return str/1
+// }
+
+// 다른사람의 풀이 2
+// function strToInt(str){
+//     return  +str;
+// }
+
+// 다른사람의 풀이 3
+// function strToInt(str){
+//     return  parseInt(str);
+// }
