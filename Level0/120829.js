7f6377958abe152ac2bb5c921be2651a636aa4fd
Jaewon <wellbird@naver.com>
2022-11-08 23:15:20 +0900

    120829 - 각도기

--- Level0/120829.js
+++ Level0/120829.js
@@ -11,3 +11,13 @@ function solution(angle) {
   }
   return answer;
 }
+
+// 다른사람의 풀이 1 - filter 사용
+// function solution(angle) {
+//     return [0, 90, 91, 180].filter(x => angle>=x).length;
+// }
+
+// 다른사람의 풀이 2 - 다중 삼항연산자 사용
+// function solution(angle) {
+//     return angle < 90 ? 1 : angle === 90 ? 2 : angle < 180 ? 3 : 4;
+// }
