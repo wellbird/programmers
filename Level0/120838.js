18fd9f6c7f8949b83e3b3c113b162f592133227f
Jaewon <wellbird@naver.com>
2023-01-04 09:47:15 +0900

    120838 - 모스부호 (1)

---
+++ Level0/120838.js
@@ -0,0 +1,36 @@
+function solution(letter) {
+  let answer = "";
+  const morse = {
+    ".-": "a",
+    "-...": "b",
+    "-.-.": "c",
+    "-..": "d",
+    ".": "e",
+    "..-.": "f",
+    "--.": "g",
+    "....": "h",
+    "..": "i",
+    ".---": "j",
+    "-.-": "k",
+    ".-..": "l",
+    "--": "m",
+    "-.": "n",
+    "---": "o",
+    ".--.": "p",
+    "--.-": "q",
+    ".-.": "r",
+    "...": "s",
+    "-": "t",
+    "..-": "u",
+    "...-": "v",
+    ".--": "w",
+    "-..-": "x",
+    "-.--": "y",
+    "--..": "z",
+  };
+  const letterArr = letter.split(" ");
+  letterArr.map((item, index) => {
+    answer += morse[item];
+  });
+  return answer;
+}
